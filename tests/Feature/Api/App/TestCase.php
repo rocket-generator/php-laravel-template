76bad02863e2edb6b14ebc8fc09f51cf59ea6502
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2024-02-11 10:21:29 +0800

    Update Template ( Add test for app apis

---
+++ tests/Feature/Api/App/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Api\App;
+
+use App\Models\User;
+use Tests\Feature\Api\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    protected string $loginAPIPath = '/api/app/auth/registration';
+
+    protected string $userID = '';
+
+    protected array $userCredential = [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $user = User::factory()->create($this->userCredential);
+        $this->loginCredential = $this->userCredential;
+    }
+
+    protected function getAuthToken(?array $credential = null): string
+    {
+        if ($credential === null) {
+            $credential = $this->loginCredential;
+        }
+        $response = $this->postJson($this->loginAPIPath, $credential, [
+            // Additional Headers
+        ]);
+
+        return $response->json('uiid');
+    }
+}
